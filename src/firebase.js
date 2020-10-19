3bca71f924c4496e01b095f3a21a35182ae692d1
= <=>
2020-10-19 14:41:14 +0200

    191020:1441

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgu_uN2iVwzzeG0qGELkofxE2_u5lY1zo",
+    authDomain: "telegram-8eb27.firebaseapp.com",
+    databaseURL: "https://telegram-8eb27.firebaseio.com",
+    projectId: "telegram-8eb27",
+    storageBucket: "telegram-8eb27.appspot.com",
+    messagingSenderId: "1097123404251",
+    appId: "1:1097123404251:web:b44569de8fc8afc0c76ffe"
+  };
